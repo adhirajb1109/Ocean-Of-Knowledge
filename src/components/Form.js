896b7d7744c0d6783dfa8e76d7e8f1a14f0b86da
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-05-20 14:56:15 +0530

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,59 @@
+import React from "react";
+function Form() {
+  function Search(event) {
+    event.preventDefault();
+    let results = document.getElementById("results");
+    let query = document.getElementById("query").value.trim();
+    let api = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${query}`;
+    fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        data.query.search.forEach((result) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+          results.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card">
+               <div class="card-body">
+                <h5 class="card-title">
+                  <a href="${url}" class="card-link">${result.title}</a>
+                </h5>
+                <a href="${url}" class=card-link" >${url}</a>
+                <h6 class="card-subtitle text-muted mt-2">${result.snippet}</span>
+                </h6>
+              </div>
+            </div>
+            <br/>`
+          );
+        });
+      });
+  }
+  return (
+    <div className="container my-4">
+      <form id="form" className="mb-4">
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="query"
+            aria-describedby="query"
+            placeholder="Search"
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          id="search"
+          onClick={Search}
+        >
+          Search
+        </button>
+      </form>
+      <div id="results"></div>
+    </div>
+  );
+}
+
+export default Form;
